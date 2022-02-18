2eeb3acc3d4a3efc2dc69e0dd07ee3e5504e1239
tranana <85484274+tranana@users.noreply.github.com>
2022-02-19 01:11:44 +0800

    1.master-chuang jian mo kuai

--- tran-increase-app/src/main/java/com/tran/increase/IncreaseApplication.java
+++ tran-increase-app/src/main/java/com/tran/increase/IncreaseApplication.java
@@ -1,9 +1,11 @@
-package com.tran.increase.app;
+package com.tran.increase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.nrsc.springstudy.c1_componentscan.config3_test")
 public class IncreaseApplication {
 
     public static void main(String[] args) {
---
+++ tran-increase-from/increase-from-api/src/main/java/com/tran/increase/config/TranConfig.java
@@ -0,0 +1,8 @@
+package com.tran.increase.config;
+
+/**
+ * @author luxiangqian
+ * 2022/2/19
+ */
+public class TranConfig {
+}
